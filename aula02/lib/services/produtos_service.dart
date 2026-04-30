cff4dd3c1ed7564b48fccdd9d2864cdd0cd88a1b
Jackeline de Paula <jackeline.delima17@gmail.com>
2026-04-29 23:18:59 -0300

    Update produtos_service.dart
    alterando codigo de produtos_service

--- aula02/lib/services/produtos_service.dart
+++ aula02/lib/services/produtos_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
-
+import 'dart:io';
 import 'package:aula02/model/produto.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 class ProdutosService {
@@ -22,6 +23,64 @@ class ProdutosService {
     return [];
   }
 
+  //cadastrar produto
+  Future<String> cadastrarProdutoComImagem({
+    required String token,
+    required Produto produto,
+    File? imagemFile,
+  }) async {
+    try {
+      final url = Uri.parse('$_baseUrl/produtos');
+
+      var request = http.MultipartRequest(
+        'POST',
+        url,
+      ); //necessario para receber dados mistos Texto e binarios(img por exemplo)
+
+      request.headers.addAll({
+        // montando requisição passo a passo
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      });
+
+      //requerindo elemento por elemento
+      request.fields['nome'] = produto.nome;
+      request.fields['descricao'] = produto.descricao;
+      request.fields['categoria_id'] = produto.categoria.toString();
+      request.fields['codigo_barras'] = produto.codigoBarras;
+      request.fields['marca'] = produto.marca;
+      request.fields['peso'] = produto.peso;
+      request.fields['ativo'] = produto.status.toString();
+
+      if (imagemFile != null) {
+        var stream = http.ByteStream(
+          imagemFile.openRead(),
+        ); //passo 1 para converter img local para o request http
+        var length = await imagemFile.length(); //length obtem o tamanho
+        var multipartFile = http.MultipartFile(
+          //cria o objeto da imagem
+          'imagem',
+          stream,
+          length,
+          filename: imagemFile.path.split('/').last,
+        );
+        request.files.add(multipartFile);
+      }
+      var response = await request.send();
+      var responseBody = await response.stream.bytesToString();
+
+      //verificando codigo de erro/acerto do codigo
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        return 'Produto cadastrado com sucesso';
+      } else {
+        Text("========= Erro ============");
+        return 'Erro: ${response.statusCode} - $responseBody';
+      }
+    } catch (e) {
+      return "Erro ao cadastrar: $e";
+    }
+  }
+
   Future<Produto> buscarProduto(int id, String token) async {
     final url = Uri.parse('$_baseUrl/produtos/$id');
     final response = await http.get(
@@ -40,21 +99,52 @@ class ProdutosService {
     }
   }
 
-  //Adicionar código para cadastrar produto em ProdutoService()
-  Future<String> cadastrarProduto(String token, Produto produto) async {
-    final url = Uri.parse('$_baseUrl/produtos');
-    final response = await http.post(
-      url,
-      headers: {
+  Future<String> atualizarProdutoComImagem({
+    required String token,
+    required int id,
+    required Produto produto,
+    File? imagemFile,
+  }) async {
+    try {
+      final url = Uri.parse('$_baseUrl/produtos/$id');
+      var request = http.MultipartRequest('POST', url);
+      request.fields['_method'] = 'PUT';
+
+      request.headers.addAll({
         'Authorization': 'Bearer $token',
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode(produto.toJson()),
-    );
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      return 'Produto cadastrado com sucesso';
-    } else {
-      return 'Erro ao cadastrar produto: ${response.statusCode} - ${response.body}';
+        'Accept': 'application/json',
+      });
+
+      request.fields['nome'] = produto.nome;
+      request.fields['descricao'] = produto.descricao;
+      request.fields['categoria_id'] = produto.categoria.toString();
+      request.fields['codigo_barras'] = produto.codigoBarras;
+      request.fields['marca'] = produto.marca;
+      request.fields['peso'] = produto.peso;
+      request.fields['ativo'] = produto.status.toString();
+
+      if (imagemFile != null) {
+        var stream = http.ByteStream(imagemFile.openRead());
+        var length = await imagemFile.length();
+        var multipartFile = http.MultipartFile(
+          'imagem',
+          stream,
+          length,
+          filename: imagemFile.path.split('/').last,
+        );
+
+        request.files.add(multipartFile);
+      }
+
+      var response = await request.send();
+      var responseBody = await response.stream.bytesToString();
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        return 'Produto atualizado com sucesso';
+      } else {
+        return 'Erro: ${response.statusCode} - $responseBody';
+      }
+    } catch (e) {
+      return 'Erro ao atualizar: $e';
     }
   }
 }
