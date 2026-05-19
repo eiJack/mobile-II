ae60f2250a10db906ab1ebaa31019866a7d3702d
Jackeline de Paula <jackeline.delima17@gmail.com>
2026-05-18 22:11:31 -0300

    alterando codigo
    Added methods to save, retrieve, and clear user information.

--- aula02/lib/services/session_service.dart
+++ aula02/lib/services/session_service.dart
@@ -1,6 +1,7 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SessionService {
+  // --------------- token -----------------
   Future<void> salvarToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString("token", token);
@@ -19,5 +20,22 @@ class SessionService {
   Future<void> limparToken() async {
     final prefs = await SharedPreferences.getInstance();
     prefs.remove('token');
+  }
+
+  // ---------------- usuario ----------------
+
+  Future<void> salvarUsuario(String usuario) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("usuario", usuario);
+  }
+
+  Future<String> obterUsuario() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString("usuario") ?? '';
+  }
+
+  Future<void> limparUsuario() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('usuario');
   }
 }
